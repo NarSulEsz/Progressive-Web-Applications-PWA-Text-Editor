845bf50d59ead4530eb4cf1713284d3195c62d9c
Nariman Suleimenov <162845160+NarSulEsz@users.noreply.github.com>
2024-09-16 18:03:19 -0400

    Added code for install.js but commented provided by instructor

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -9,3 +9,30 @@ butInstall.addEventListener('click', async () => {});
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {});
+
+/* Instructor provided solution 2024-09-16 */
+
+/*const butInstall = document.getElementById('buttonInstall');
+
+window.addEventListener('beforeinstallprompt', (event) => {
+  window.deferredPrompt = event;
+  butInstall.classList.toggle('hidden', false);
+});
+
+butInstall.addEventListener('click', async () => {
+  const promptEvent = window.deferredPrompt;
+
+  if (!promptEvent) {
+    return;
+  }
+
+  promptEvent.prompt();
+
+  window.deferredPrompt = null;
+
+  butInstall.classList.toggle('hidden', true);
+});
+
+window.addEventListener('appinstalled', () => {
+  window.deferredPrompt = null;
+}); */
